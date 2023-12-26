f1282be646739190ce6bdadf201ea2c550707dd8
lglavanya <lavanyakolar1999@gmail.com>
2023-12-26 12:02:18 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var helloWorldElement = document.getElementById("helloWorld");
+    helloWorldElement.innerHTML = "Hello, World! (Updated by JavaScript)";
+});
+
